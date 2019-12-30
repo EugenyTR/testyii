71e0468335f13ae0514fd82b853c4d7a7f489743
EugenyTR <55189944+EugenyTR@users.noreply.github.com>
2019-12-30 11:39:43 +0700

    run yii application

---
+++ config/web.php
@@ -0,0 +1,8 @@
+<?php
+
+    return [
+        'id' => 'testyii',
+        'basePath' => realpath(__DIR__ . '/../')
+    ];
+
+?>
--- web/index.php
+++ web/index.php
@@ -1,5 +1,7 @@
 <?php
 
-    echo "<h1>Testing Yii2</h1>";
+    require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+    $config = require __DIR__ . '/../config/web.php';
+    (new yii\web\Application($config)) -> run();
 
 ?>
