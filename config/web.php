8ffcbd13123ac8fb77148cb57f554e12ae8739ad
EugenyTR <55189944+EugenyTR@users.noreply.github.com>
2019-12-30 23:01:37 +0700

    add join and login views + htaccess

--- config/web.php
+++ config/web.php
@@ -2,7 +2,13 @@
 
     return [
         'id' => 'testyii',
-        'basePath' => realpath(__DIR__ . '/../')
+        'basePath' => realpath(__DIR__ . '/../'),
+        'components' => [
+            'urlManager' => [
+                'enablePrettyUrl' => true,
+                'showScriptName' => false
+            ]
+        ]
     ];
 
 ?>
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,5 +1,6 @@
 <?php
 
+use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 
 ?>
@@ -19,6 +20,14 @@ use yii\bootstrap\NavBar;
             'options' => [
                 'class' => 'navbar-default navbar-fixed-top'
             ]
+        ]);
+        $menu = [
+            ['label' => 'Join', 'url' => ['/site/join']],
+            ['label' => 'Login', 'url' => ['/site/login']]
+        ];
+        echo Nav::widget([
+           'options' => ['class' => 'navbar-nav navbar-right'],
+            'items' => $menu
         ]);
         NavBar::end();
     ?>
