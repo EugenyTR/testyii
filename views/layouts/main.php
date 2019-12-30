981dec2c64c448fc2476339b4e5c6d21abedd478
EugenyTR <55189944+EugenyTR@users.noreply.github.com>
2019-12-30 18:26:05 +0700

    add boostrap navbar

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,9 +1,32 @@
+<?php
+
+use yii\bootstrap\NavBar;
+
+?>
+<?php $this->beginPage(); ?>
 <html>
     <head>
         <title>Test Yii2</title>
+        <?php $this->head(); ?>
+
     </head>
     <body>
+    <?php $this->beginBody(); ?>
+    <?php
+        NavBar::begin([
+            brandLabel => 'Test Yii2',
+            'brandUrl' => Yii::$app->homeUrl,
+            'options' => [
+                'class' => 'navbar-default navbar-fixed-top'
+            ]
+        ]);
+        NavBar::end();
+    ?>
+    <div class="container" style="margin-top: 80px">
         <?= $content ?>
+    </div>
+    <?php $this->endBody(); ?>
 
     </body>
 </html>
+<?php $this->endPage(); ?>
