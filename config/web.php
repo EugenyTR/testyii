db9ec45ed189ac1e630f7f85c70fc2736ecd909b
EugenyTR <55189944+EugenyTR@users.noreply.github.com>
2020-01-01 21:30:18 +0700

    add debug line

--- config/web.php
+++ config/web.php
@@ -3,10 +3,16 @@
     return [
         'id' => 'testyii',
         'basePath' => realpath(__DIR__ . '/../'),
+        'bootstrap' => ['debug'],
         'components' => [
             'urlManager' => [
                 'enablePrettyUrl' => true,
                 'showScriptName' => false
+            ]
+        ],
+        'modules' => [
+            'debug' => [
+                'class' => 'yii\debug\Module'
             ]
         ]
     ];
