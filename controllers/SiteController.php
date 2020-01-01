f041c070113ad2771349649daeec399b6d5fe149
EugenyTR <55189944+EugenyTR@users.noreply.github.com>
2020-01-01 21:50:12 +0700

    add join and login action to user controller

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -10,14 +10,4 @@ class SiteController extends Controller
     {
         return $this->render('index');
     }
-
-    public function actionJoin ()
-    {
-        return $this->render('join');
-    }
-
-    public function actionLogin ()
-    {
-        return $this->render('login');
-    }
 }
